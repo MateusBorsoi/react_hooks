75cf4fee10800f7d13f89c6dfa0bc4152b974d5f
Mateus Borsoi <borsoimateus@gmail.com>
2023-05-12 15:44:51 -0300

    context

--- src/routes/DataContext.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export const Data = {
-    number: 123,
-    text: 'context'
-}
-
-export const DataContext = React.createContext(Data)
--- src/routes/UseContext.jsx
+++ src/routes/UseContext.jsx
@@ -1,7 +1,15 @@
+import { useContext } from "react"
+import DataContext from "../data/DataContext"
+
 const UseContext = () => {
+
+   const context = useContext(DataContext)
+
     return (
         <div>
             <h3>Use Context</h3>
+            <p><span>{context.text}</span></p>
+            <p><span>{context.number}</span></p>
         </div>
     )
 }
